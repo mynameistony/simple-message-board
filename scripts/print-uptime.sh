ea4fc8f53fc6b1713c6b333734ceeec5b61b9a47
Tony <tonyroger10195@gmail.com>
2015-03-12 14:47:38 -0700

    that SHOULD fix the uptime

--- scripts/print-uptime.sh
+++ scripts/print-uptime.sh
@@ -1,3 +1,3 @@
-thisUptime=$(/usr/bin/uptime | grep "up.*,  1 user" -o | sed s/",  1 user"// | sed s/":"/" hours, "/ | sed s/",$"/" minutes"/ | sed s/"up "//)
+thisUptime=$(/usr/bin/uptime | grep "up.*,.*user" -o | sed s/",  .*user$"// | sed s/":"/" hours, "/ | sed s/",$"/" minutes"/ | sed s/"up "//)
 
 echo "I've been up for $thisUptime!"
