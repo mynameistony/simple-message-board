a641ed36bad9eee039e643f6cbc62ded89df9d8e
Tony <tonyroger10195@gmail.com>
2015-03-12 19:26:11 +0000

    fixed print-uptime

--- scripts/print-uptime.sh
+++ scripts/print-uptime.sh
@@ -1,3 +1,3 @@
-thisUptime=$(/usr/bin/uptime | grep "up.*[0-9]*:[0-9]*," -o | sed s/":"/" hours, "/ | sed s/"$"/" minutes"/ | sed s/"up "//)
+thisUptime=$(/usr/bin/uptime | grep "up.*[0-9]*:[0-9]*," -o | sed s/":"/" hours, "/ | sed s/",$"/" minutes"/ | sed s/"up "//)
 
 echo "I've been up for $thisUptime!"
