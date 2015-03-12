ce495b8e316c5a6164d486ab789ba0241cb487a9
Tony <tonyroger10195@gmail.com>
2015-03-12 16:20:30 -0700

    god damn

--- scripts/output-footer.sh
+++ scripts/output-footer.sh
@@ -2,7 +2,8 @@
 echo "<p class=\"uptime\" id=\"uptime\">$(scripts/print-uptime.sh)</p>"
 
 storage=$(df -h /home/pi/Storage2 | grep "[0-9]*%" -o | grep "[0-9]")
-echo "<p> class=\"storage-left\" id=\"storage-left\">$storage of storage used</p>"
+
+echo "<p class=\"storage-left\" id=\"storage-left\">$storage of storage used</p>"
 
 echo "<p class=\"time-stamp\" id=\"date\">$(date '+%A, %B %d %Y%n%r')</p>"
 
